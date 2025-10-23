4719037e41159bca2e87a4ffde8f38e9ca8fed12
LeviMa1 <levi@onesystecnologia.com>
2025-10-23 16:58:20 -0300

    att

---
+++ rest-with-spring-boot-and-java-erudio/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,6 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'São Paulo - Brasil', 'Ayrton', 'Male', 'Senna'),
+	(2, 'Anchiano - Italy', 'Leonardo', 'Male', 'da Vinci'),
+	(6, 'Mvezo - South Africa', 'Nelson', 'Male', 'Mandela'),
+	(7, 'Mvezo - South Africa', 'Nelson', 'Male', 'Mandela'),
+	(8, 'Smiljan - Croatia', 'Nikola', 'Male', 'Tesla');
